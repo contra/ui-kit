41097b77f4a6d959d2d8caef091af36c0fd67609
Eric Taylor <eric.taylor@contra.com>
2021-05-11 12:16:31 -0600

    feat: setup development tools

---
+++ .lintstagedrc.js
@@ -0,0 +1,34 @@
+const { ESLint } = require('eslint');
+
+const eslintCli = new ESLint();
+
+const filterAsync = async (array, callback) => {
+  const results = await Promise.all(
+    array.map(async (value, index) => {
+      const result = await callback(value, index);
+
+      return result;
+    })
+  );
+
+  return array.filter((_, i) => results[i]);
+};
+
+const removeIgnoredFiles = async (files) => {
+  const filteredFiles = await filterAsync(files, async (file) => {
+    const isIgnored = await eslintCli.isPathIgnored(file);
+
+    return !isIgnored;
+  });
+
+  return filteredFiles.join(' ');
+};
+
+module.exports = {
+  '*.{js,jsx,ts,tsx,md,html,css}': 'prettier --write',
+  '*.{js,jsx,ts,tsx}': async (files) => {
+    const filesToLint = await removeIgnoredFiles(files);
+
+    return [`eslint --max-warnings=0 --fix ${filesToLint}`];
+  },
+};
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+/**
+ * Options for Prettier.
+ *
+ * @see https://prettier.io/docs/en/options.html
+ */
+
+module.exports = {
+  singleQuote: true,
+};
