02ff98e6e8285884aeb5d7c8fa76f454838d54b9
Eric Taylor <eric.taylor@contra.com>
2021-11-14 15:32:28 -0700

    feat: do not ship as type:module in package

--- bin/prepare.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-# Shell script to write property to package.json file
-jq -e '.type = "module"' package.json > package.json.tmp && cp package.json.tmp package.json && rm package.json.tmp
