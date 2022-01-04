be44b48292b3a5246553cc9695d8f52c4d94abcf
Luke Bailey <hello@lukebailey.io>
2022-01-04 23:13:31 +0000

    feat: add space system to stitches config

--- stitches.config.ts
+++ stitches.config.ts
@@ -14,6 +14,17 @@ export const {
   media: {},
   theme: {
     colors: colorPrimitives,
+    space: {
+      4: '4px',
+      8: '8px',
+      16: '16px',
+      24: '24px',
+      32: '32px',
+      48: '48px',
+      64: '64px',
+      96: '96px',
+      128: '128px',
+    },
   },
   utils: {},
 });
