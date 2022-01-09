969c328f96af5d444b9a102c4bc053906945137c
Luke Bailey <hello@lukebailey.io>
2022-01-09 22:01:24 +0000

    feat: add animatiable check icon

---
+++ src/animationIcons/CheckIcon.tsx
@@ -0,0 +1,27 @@
+import type { SVGProps } from 'react';
+import { styled } from '../../stitches.config';
+
+const Path = styled('path', {
+  transition: 'stroke-dashoffset 0.4s cubic-bezier(0.16, 1, 0.3, 1)',
+});
+
+export const CheckIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    fill="none"
+    focusable="false"
+    height="1em"
+    role="img"
+    viewBox="0 0 16 16"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <Path
+      d="M0.709339 7.44716L5.70834 12.4462L15.3004 2.8541"
+      stroke="currentColor"
+      strokeDasharray="21 21"
+      strokeMiterlimit="10"
+      strokeWidth="2"
+    />
+  </svg>
+);
