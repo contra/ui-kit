3ce91982afa6694d7a7f57cee77387cc2f72fbf2
Eric Taylor <eric.taylor@contra.com>
2022-02-09 00:41:58 -0700

    refactor: simplify typing for BaseTheme

--- src/theme/types.ts
+++ src/theme/types.ts
@@ -13,38 +13,29 @@ export type ThemeScaleObject = {
   [key: string]: ThemeScaleObject | string;
 };
 
-/** Configuration Interface */
-// eslint-disable-next-line @typescript-eslint/no-namespace
-declare namespace ConfigType {
-  /** Theme interface. */
-  export type Theme<T = {}> = {
-    [Scale in keyof T]: {
-      [Token in keyof T[Scale]]: T[Scale][Token] extends
-        | boolean
-        | number
-        | string
-        ? T[Scale][Token]
-        : boolean | number | string;
-    };
-  } & {
-    borderStyles?: { [token in number | string]: boolean | number | string };
-    borderWidths?: { [token in number | string]: boolean | number | string };
-    colors?: { [token in number | string]: boolean | number | string };
-    fontSizes?: { [token in number | string]: boolean | number | string };
-    fontWeights?: { [token in number | string]: boolean | number | string };
-    fonts?: { [token in number | string]: boolean | number | string };
-    letterSpacings?: { [token in number | string]: boolean | number | string };
-    lineHeights?: { [token in number | string]: boolean | number | string };
-    radii?: { [token in number | string]: boolean | number | string };
-    shadows?: { [token in number | string]: boolean | number | string };
-    sizes?: { [token in number | string]: boolean | number | string };
-    space?: { [token in number | string]: boolean | number | string };
-    transitions?: { [token in number | string]: boolean | number | string };
-    zIndices?: { [token in number | string]: boolean | number | string };
+/** Theme interface. */
+export type BaseTheme<T = {}> = {
+  [Scale in keyof T]: {
+    [Token in keyof T[Scale]]: T[Scale][Token] extends boolean | number | string
+      ? T[Scale][Token]
+      : boolean | number | string;
   };
-}
-
-export type BaseTheme = ConfigType.Theme;
+} & {
+  borderStyles?: { [token in number | string]: boolean | number | string };
+  borderWidths?: { [token in number | string]: boolean | number | string };
+  colors?: { [token in number | string]: boolean | number | string };
+  fontSizes?: { [token in number | string]: boolean | number | string };
+  fontWeights?: { [token in number | string]: boolean | number | string };
+  fonts?: { [token in number | string]: boolean | number | string };
+  letterSpacings?: { [token in number | string]: boolean | number | string };
+  lineHeights?: { [token in number | string]: boolean | number | string };
+  radii?: { [token in number | string]: boolean | number | string };
+  shadows?: { [token in number | string]: boolean | number | string };
+  sizes?: { [token in number | string]: boolean | number | string };
+  space?: { [token in number | string]: boolean | number | string };
+  transitions?: { [token in number | string]: boolean | number | string };
+  zIndices?: { [token in number | string]: boolean | number | string };
+};
 
 export type ThemeType = 'light';
 
