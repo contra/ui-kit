460cfd0c3c1df0865c480466f0917fa174cc4ed5
Eric Taylor <eric.taylor@contra.com>
2022-04-05 16:35:00 -0600

    feat(icons): update icons and icon variants

---
+++ src/icons/CreditCardSolidIcon.tsx
@@ -0,0 +1,20 @@
+import type { SVGProps } from 'react';
+export const CreditCardSolidIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    focusable="false"
+    role="img"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M0 7.5c.001-1.483.02-2.26.327-2.862a3 3 0 011.311-1.311C2.28 3 3.12 3 4.8 3h14.4c1.68 0 2.52 0 3.162.327a3 3 0 011.311 1.311c.307.602.326 1.38.327 2.862H0zM0 9v7.2c0 1.68 0 2.52.327 3.162a3 3 0 001.311 1.311C2.28 21 3.12 21 4.8 21h14.4c1.68 0 2.52 0 3.162-.327a3 3 0 001.311-1.311C24 18.72 24 17.88 24 16.2V9H0zm7 2.5A2.942 2.942 0 009.5 14v1A2.942 2.942 0 007 17.5H6A2.942 2.942 0 003.5 15v-1A2.942 2.942 0 006 11.5h1z"
+      fill="#000"
+    />
+  </svg>
+);
